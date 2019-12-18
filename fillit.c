bbf6a8f41a03e54a54887620433be9b853b0700b
Svyatoslav Krasin <skrasin@e1z2r4p9.42.us.org>
2019-12-17 18:35:33 -0800

    26 fix

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: skrasin <skrasin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 13:45:32 by skrasin           #+#    #+#             */
-/*   Updated: 2019/12/16 13:13:22 by skrasin          ###   ########.fr       */
+/*   Updated: 2019/12/17 18:35:18 by skrasin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,10 +60,8 @@ int			main(int argc, char **argv)
 			error(2);
 		n = 0;
 		tab = reader(fd, &n);
-		if (tab)
+		if (tab && n < 27)
 		{
-			if (n > 26)
-				error(2);
 			close(fd);
 			ft_solve(tab, ft_isqrtc(n * 4));
 			list_dell(&tab);
